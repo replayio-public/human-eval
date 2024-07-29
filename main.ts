9bd719124bcbcfb63dc9c180c389be3213fc2004
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-29 23:25:21 +0200

    loopy loop

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
 import * as fs from "fs/promises";
 
 interface HumanEvalProblem {
@@ -11,13 +13,19 @@ interface HumanEvalProblem {
 
 async function main() {
   const evalProblems = await fs.readFile("data.jsonl");
-  console.log(
-    evalProblems
-      .toString()
-      .split("\n")
-      .filter((x) => x.length)
-      .map((x) => JSON.parse(x)),
-  );
+  const problems = evalProblems
+    .toString()
+    .split("\n")
+    .filter((x) => x.length)
+    .map((x) => JSON.parse(x)) as HumanEvalProblem[];
+
+  for (const p of problems) {
+    const answer = await generateText({
+      model: openai("gpt-3.5-turbo"),
+      prompt: p.prompt,
+    });
+    eval(p.prompt + " " + answer.text + "\n" + p.test);
+  }
 }
 
 main();
